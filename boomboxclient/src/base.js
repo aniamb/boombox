284ca2a6a4a09d10493f73b65f06eafb9f183416
Netra Pradhan <npradha@purdue.edu>
2019-04-18 09:36:19 -0400

    Add firebase database

---
+++ boomboxclient/src/base.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD2-jP3vyKiZDGWu3IsiKTaagdx0R5wjDo",
+    authDomain: "boombox-1c217.firebaseapp.com",
+    databaseURL: "https://boombox-1c217.firebaseio.com",
+    projectId: "boombox-1c217",
+    storageBucket: "",
+    messagingSenderId: "129261596427"
+  };
+  firebase.initializeApp(config);
